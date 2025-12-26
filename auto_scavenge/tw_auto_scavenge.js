8e78d2b5a4b3b26cc70a30641dd304d8fefc23e1
Eric N. Cordeiro <eric.cordeiro@fcxlabs.com.br>
2025-12-26 18:12:34 -0300

    feat: [auto_scavenge] commit inicial

---
+++ auto_scavenge/tw_auto_scavenge.js
@@ -0,0 +1,17 @@
+// Script de automação de coleta para o jogo Tribal Wars
+// Autor: Éric N. Cordeiro
+// Data: 27 de junho de 2024
+// Versão: 1.0
+// Descrição: Este script realiza automações de coleta no jogo Tribal Wars.
+
+// Função principal de automação de coleta
+function autoScavenge() {
+  scavenge();
+}
+
+// Função para realizar a coleta
+function scavenge() {
+  UI.SuccessMessage("Iniciando a coleta automática...");
+}
+
+autoScavenge();
